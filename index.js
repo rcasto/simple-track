5dfcfec502cce6b90739bd788845f217d63cfdf6
Richie Casto <rcasto92@gmail.com>
2020-12-30 17:43:03 -0500

    add some exports to allow use as module as well

--- index.js
+++ index.js
@@ -44,7 +44,7 @@
  * https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundamental-exercise-253.php
  * @returns {string} A string representing a UUID
  */
-function generateUUID() {
+export function generateUUID() {
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
         (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
     );
@@ -76,7 +76,7 @@ function createEvent({
  * @param {EventGeneratorInfo} eventGeneratorInfo
  * @returns {EventGenerator}
  */
-function createEventGenerator({
+export function createEventGenerator({
     appName,
     analyticsApiUrl,
     sessionStorageKey = 'analytics-session-id',
