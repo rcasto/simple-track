ff739d935f7c215eaa9876b266f16b8775cc05f6
rcasto <rcasto92@gmail.com>
2023-01-02 16:23:17 -0500

    add usage of crypto.randomUUID() if available

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import { IEvent, IEventGenerator, IEventGeneratorInfo, IEventInfo } from "./sche
  * Generates a UUID utilizing crypto.getRandomValues underneath. This method was taken from:
  * - https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundamental-exercise-253.php
  * - https://stackoverflow.com/a/2117523
- * 
- * There is a UUID standard proposal here:
- * - https://github.com/WICG/uuid
  * @returns {string} A string representing a UUID
  */
 export function generateUUID(): string {
+    if (typeof crypto.randomUUID === 'function') {
+        return crypto.randomUUID();
+    }
+
     return ('' + [1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c: string) =>
         (c as unknown as number ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c as unknown as number / 4)))).toString(16)
     );
